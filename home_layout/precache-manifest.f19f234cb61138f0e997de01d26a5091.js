e79ed35e4a78c1656aab82b54fb99d0d28af2f5e
王亚辉 <bjwangyahui@corp.netease.com>
2019-03-04 18:08:08 +0800

    feat: 上传房间布局

---
+++ home_layout/precache-manifest.f19f234cb61138f0e997de01d26a5091.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "cd9813858b09af7222df",
+    "url": "/js/chunk-vendors.475f8607.js"
+  },
+  {
+    "revision": "e498ef9ef89fed30c236",
+    "url": "/js/app.147283c6.js"
+  },
+  {
+    "revision": "48af24cd7dae7280ac56",
+    "url": "/js/about.41d636ee.js"
+  },
+  {
+    "revision": "405c9b7c4a4abdd0c2c2088687c082f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e498ef9ef89fed30c236",
+    "url": "/css/app.57b4841c.css"
+  }
+];
